465b4660c2014697bf9d8c29bbc05c38147ed266
ineffabledeeps <ineffabledeeps@gmail.com>
2022-08-10 00:56:17 +0530

    complete sync

--- model_name/api/views.py
+++ model_name/api/views.py
@@ -7,7 +7,6 @@ from rest_framework.response import Response
 from rest_framework import permissions
 from model_name.models import classroom, timetable,dept
 from rest_framework.views import APIView
-from rest_framework.parsers import JSONParser
 from model_name.models import *
 from model_name.models import faculties
 from datetime import date
